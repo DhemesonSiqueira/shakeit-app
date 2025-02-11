1417c6b9eef49a36a1c69ef42efebf02af2e4366
DhemesonSiqueira <dhemeson_loran@hotmail.com>
2025-02-10 22:20:46 -0300

    refactor: extract TrendingMovies component into a separate file

---
+++ src/components/TrendingMovies.jsx
@@ -0,0 +1,21 @@
+function TrendingMovies({ trendingMovies }) {
+  return (
+    <ul>
+      {trendingMovies.map((movie, index) => (
+        <li key={movie.id}>
+          <p>{index + 1}</p>
+          <img
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : "./no-movie.png"
+            }
+            alt={movie.title}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TrendingMovies;
